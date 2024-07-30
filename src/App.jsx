8ac6e3cf4a7d39f229d30affe2280a1110f12efd
pritam dutta <webdevpritam7498@gmail.com>
2024-07-31 02:14:08 +0600

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 function App() {
+  let foods = ["apple", "mango", "jackfruit", "bannana", "milk"];
   return (
     <center class="container">
       <h1 class="text-primary">Todo App</h1>
       <br />
-      <div>
-        <input type="text" id="taskInput" placeholder="Enter Todo Here" />
-        <input type="date" id="taskInput" placeholder="" />
-      </div>
+
+      <ul class="list-group">
+        {foods.map((item) => (
+          <li class="list-group-item">{item}</li>
+        ))}
+      </ul>
     </center>
   );
 }
